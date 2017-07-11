3ba77036fb02ec5603bdef3846cb67878bba0de1
qingdai <qingdai54@gmail.com>
2017-07-11 14:40:50 -0700

    output Gray_image

--- mexican_hat.m
+++ mexican_hat.m
@@ -1,7 +1,7 @@
-function Binary_mask = mexican_hat(I, Size, Sigma, Std)
+function [Binary_mask, Gray_image] = mexican_hat(I, Size, Sigma, Std)
 
 
-
+%I_bw is a grayscale image, not BW
 I_bw = mat2gray(I);
 
 %pad image with zeros
@@ -14,15 +14,30 @@ I_hat = mexconv(I_pad,mex_hat);
 %Create a threshold. Take the mean of the of the whole image and find the
 %standard deviation
 
-%converting image matrix to a vector
+
 sz_image = size(I_hat);
 img_vector = reshape(I_hat, [1 sz_image(1)*sz_image(2)]);
 avg_image = mean(img_vector);
 std_image = std(img_vector);
+
+% Create a binary image
 Binary_mask = I_hat > (avg_image + Std * std_image);
 
-%removing padding of image
+%removing padding of image for mask and gray image 
 Binary_mask(1:70,:)=[];
 Binary_mask(end-69:end,:)= [];
 Binary_mask(:,1:70)=[];
 Binary_mask(:,end-69:end)=[];
+
+% Create a gray image and remove padding
+Gray_image = I_hat ;
+
+Gray_image(1:70,:)=[];
+Gray_image(end-69:end,:)= [];
+Gray_image(:,1:70)=[];
+Gray_image(:,end-69:end)=[];
+
+%normalize
+Gray_image = mat2gray(Gray_image);
+%converting image matrix to a vector
+
