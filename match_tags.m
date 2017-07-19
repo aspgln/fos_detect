07345e7947d4ceae98560fe07f3d228830b328bc
qingdai <qingdai54@gmail.com>
2017-07-19 15:30:19 -0700

    use load_tags

--- match_tags.m
+++ match_tags.m
@@ -1,9 +1,9 @@
-function [num_of_positive_signals,positive_signals] = match_tags(import_tags, Candidate_properties)
+function [num_of_positive_signals,positive_signals] = match_tags(load_tags, Candidate_properties)
 
 
 
 %compared centroid and tags in two different colors
-num_of_positive_signals = length(import_tags);
+num_of_positive_signals = length(load_tags);
 
 centroids = {Candidate_properties.Centroid};
 
@@ -16,11 +16,11 @@ positive_signals = [];
 all_norms = zeros(num_of_positive_signals, 1);
 
 
-for i = 1:length(import_tags)
+for i = 1:length(load_tags)
     eu_norm = zeros(length(centroids),1);
 
     for j = 1:length(centroids)
-        eu_norm(j) = norm(import_tags(i,2:3) -centroids{j});
+        eu_norm(j) = norm(load_tags(i,2:3) -centroids{j});
     end
     
     [min_norm,index] = min(eu_norm);
