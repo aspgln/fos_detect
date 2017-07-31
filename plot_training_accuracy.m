4dc9f2c42bfa139b012ba9130194d0cd6aa30ba5
qingdai <qingdai54@gmail.com>
2017-07-31 16:31:02 -0700

    plot accuracy

---
+++ plot_training_accuracy.m
@@ -0,0 +1,15 @@
+function plot_training_accuracy(info)
+
+persistent plotObj
+
+if info.State == "start"
+    figure;
+    plotObj = animatedline;
+    xlabel("Iteration")
+    ylabel("Training Accuracy")
+elseif info.State == "iteration"
+    addpoints(plotObj,info.Iteration,info.TrainingAccuracy)
+    drawnow limitrate nocallbacks
+end
+
+end
