f143e941b5f18463fe645e7f445aa5186c8beaff
qingdai <qingdai54@gmail.com>
2017-07-31 16:31:37 -0700

    stop training

---
+++ stop_training_at_threshold.m
@@ -0,0 +1,20 @@
+function stop = stop_training_at_threshold(info,thr)
+
+stop = false;
+if info.State ~= "iteration"
+    return
+end
+
+persistent iterationAccuracy
+
+% Append accuracy for this iteration
+iterationAccuracy = [iterationAccuracy info.TrainingAccuracy];
+
+% Evaluate mean of iteration accuracy and remove oldest entry
+if numel(iterationAccuracy) == 50
+    stop = mean(iterationAccuracy) > thr;
+    
+    iterationAccuracy(1) = [];
+end
+
+end
