1f0bf7eec652c306293bebe5ceb2b701f1a98ade
qingdai <qingdai54@gmail.com>
2017-08-08 16:57:13 -0700

    multi-select, using whole data-set

--- main_adaboost.m
+++ main_adaboost.m
@@ -1,277 +1,150 @@
 %% extract features
 
-counter = 0;
-answer = 'y';
 
-cfos_feature_vector = [];
-cfos_label_vector = [];
-tdt_feature_vector = [];
-tdt_label_vector = [];
+Feature_vector = [];
+Label_vector = [];
 
-while answer == 'y'
-    counter = counter + 1;
-    disp(counter)
-%     target = ['cfos', 'tdt'];
-    
-    [filename,pathname] = uigetfile('../images/new/train/*.tif', 'Select cfos image file');
-    cfos_image_path = [pathname, filename];
-    disp(filename)
-    
-%     [filename,pathname] = uigetfile('../images/*.tif', 'Select tdt image file');
-%     tdt_image_path = [pathname, filename];
-    
-    [filename,pathname] = uigetfile('../images/new/train/*.xlsx', 'Select tag file');
-    tag_path = [pathname, filename];
-    disp(filename)
-    
-    [cfos_features, cfos_labels] = extract_feature_and_import_tags(cfos_image_path, tag_path, 'cfos');
-    
-    cfos_feature_vector = [cfos_feature_vector; cfos_features];
-    cfos_label_vector = [cfos_label_vector; cfos_labels];
 
-%     [tdt_features, tdt_labels] = extract_feature(tdt_image_path, tag_path, 'tdt');
-%     tdt_feature_vector = [tdt_feature_vector; tdt_features];
-%     tdt_label_vector = [tdt_label_vector; tdt_labels];
-    
-    answer = input('more training images? y or n : ', 's');
 
+% select multiple image and tag files2
+% [filename,pathname] = uigetfile('../images/new/DH/data/cfos/*.tif', ...
+%     'Select image file', 'MultiSelect', 'on' );
+[filename,pathname] = uigetfile('../data/DH/cfos/*.tif', ...
+   'Select image file', 'MultiSelect', 'on' );
+% store file paths in a vector
+cfos_image_path_vector = strcat(pathname, filename(:)); 
+
+
+[filename,pathname] = uigetfile('../data/DH/tag/*.xlsx', ...
+    'Select image file', 'MultiSelect' , 'on');
+tag_path_vector = strcat(pathname, filename(:));
+
+
+% extract patches from each image, extract labels
+l = length(filename);
+tic
+for i = 1: l
+    % track time
+    remain_time = (toc / i) * (l-i);
+    
+    s = sprintf(' %d / %d \n time used: %.2f \n time remains %.2f \n', i,l, toc, remain_time);    fprintf(s);   
+    fprintf(s);  
+    
+     [Features, Labels] = extract_feature_and_import_tags...
+         (cfos_image_path_vector{i}, tag_path_vector{i}, 'cfos');
+    
+    
+    
+    Feature_vector = [Feature_vector; Features];
+    Label_vector = [Label_vector;  Labels];      
+    
 end
+
+
+%% cross validation
+
+m = length(Feature_vector);
+
+% find the indices of all positive signals
+positive_index = find(Label_vector);
+
+% find the indices of all negative signals
+negative_index = setdiff(1:m, positive_index);
+
+% randomly choose subsamples to balance data
+negInd = randsample(negative_index, length(positive_index));
+
+% combine and shuffle subsample
+total_index = [positive_index; negInd'];
+total_index = total_index(randperm(length(total_index)));
+
+% set k-fold validation
+k = 5;
+cv = cvpartition(total_index, 'kfold', k);
+
+
+
 %% train images 
 
+result = zeros(6,k);
 
-
-
-adaboost_train_features = cfos_feature_vector;
-adaboost_train_labels = cfos_label_vector;
-
-adaboost_train_labels (adaboost_train_labels == 0) =  -1;
-1
-[estimateclasstotal,model_adaboost_1] = adaboost('train',adaboost_train_features,adaboost_train_labels,10);
-2
-[estimateclasstotal,model_adaboost_2] = adaboost('train',adaboost_train_features,adaboost_train_labels,20);
-3
-[estimateclasstotal,model_adaboost_3] = adaboost('train',adaboost_train_features,adaboost_train_labels,30);
-4
-[estimateclasstotal,model_adaboost_4] = adaboost('train',adaboost_train_features,adaboost_train_labels,50);
-5
-[estimateclasstotal,model_adaboost_5] = adaboost('train',adaboost_train_features,adaboost_train_labels,100);
-
-
-%% test images
-
- [filename,pathname] = uigetfile('../images/new/test/*.tif', 'Select image file');
-    cfos_test_image_path = [pathname, filename]
-
-    % [filename,pathname] = uigetfile('../images/*.tif', 'Select image file');
-    % tdt_test_image_path = [pathname, filename];
-
-    [filename,pathname] = uigetfile('../images/new/test/*.xlsx', 'Select tag file');
-    test_tag_path = [pathname, filename];
-
-    [cfos_test_feature_vector, cfos_test_label_vector] = extract_feature_and_import_tags(cfos_test_image_path, test_tag_path, 'cfos');
- 
-
-
-
-    % [tdt_test_feature_vector, tdt_test_label_vector] = extract_feature_and_import_tags(tdt_test_image_path, test_tag_path, 'tdt');
-
-
-%% test model
+for i = 1:k 
+    disp(i)
+    % divide into two subsets by indices
+    trainInd = total_index(training(cv,i));
+    testInd  = total_index(test(cv,i));
     
-
+    
+    trainData = struct('Features', Feature_vector(trainInd(1:end), :),...
+        'Label', Label_vector(trainInd(1:end)));
+    testData = struct('Features', Feature_vector(testInd(1:end), :),...
+        'Label', Label_vector(testInd(1:end)));
+    
+    trainData.Label(trainData.Label == 0) =  -1;
+    
+  
+    
+    % train 
+    
+    adaboost_train_features = trainData.Features;
+    adaboost_train_labels = trainData.Label;
+    
+%     adaboost_train_labels (adaboost_train_labels == 0) =  -1;
+    
+    [estimateclasstotal,model_adaboost_1] = adaboost('train',adaboost_train_features,adaboost_train_labels,10);
+    
+    
+    
+    % test
+    cfos_test_feature_vector = testData.Features;
     adaboost_predict_label_1 = adaboost('apply',cfos_test_feature_vector,model_adaboost_1);
-    adaboost_predict_label_2 = adaboost('apply',cfos_test_feature_vector,model_adaboost_2);
-    adaboost_predict_label_3 = adaboost('apply',cfos_test_feature_vector,model_adaboost_3);
-    adaboost_predict_label_4 = adaboost('apply',cfos_test_feature_vector,model_adaboost_4);
-    adaboost_predict_label_5 = adaboost('apply',cfos_test_feature_vector,model_adaboost_5);
 
-    
     adaboost_predict_label_1(adaboost_predict_label_1 == -1) = 0;
-    adaboost_predict_label_2(adaboost_predict_label_2 == -1) = 0;
-    adaboost_predict_label_3(adaboost_predict_label_3 == -1) = 0;
-    adaboost_predict_label_4(adaboost_predict_label_4 == -1) = 0;
-    adaboost_predict_label_5(adaboost_predict_label_5 == -1) = 0;
     
-
-%% accuracy 
-
-tp = 0;
-fp = 0;
-fn = 0;
-tn = 0;
+    
+    
+    cfos_test_label_vector = testData.Label;
 
 
-for i = 1:length(adaboost_predict_label_1)
-    if (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 1)
-        tp = tp + 1;
-    elseif (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 0)
-        fn = fn + 1;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 1)
-        fp = fp + 1    ;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 0)
-        tn = tn + 1;
-    end 
-        
+    tp = 0;
+    fp = 0;
+    fn = 0;
+    tn = 0;
+
+
+    for j = 1:length(adaboost_predict_label_1)
+        if (cfos_test_label_vector(j) == 1) && (adaboost_predict_label_1(j) == 1)
+            tp = tp + 1;
+        elseif (cfos_test_label_vector(j) == 1) && (adaboost_predict_label_1(j) == 0)
+            fn = fn + 1;
+        elseif (cfos_test_label_vector(j) == 0) && (adaboost_predict_label_1(j) == 1)
+            fp = fp + 1    ;
+        elseif (cfos_test_label_vector(j) == 0) && (adaboost_predict_label_1(j) == 0)
+            tn = tn + 1;
+        end 
+
+    end
+
+    precision = tp / (tp + fp);
+
+    recall =  tp / (tp + fn);
+
+    accuracy = (tp + tn) / (tp + tn + fp + fn );
+
+
+    x = {'model 1', ''; 
+        'tp', tp; 'fp', fp; 'fn', fn;
+        'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
+
+
+    r = cell2mat(x(2:7,2));
+   disp(r);
+  result(:, i) = r;		
+  
+  
 end
 
-precision = tp / (tp + fp);
-
-recall =  tp / (tp + fn);
-
-accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-x = {'model 1', ''; 
-    'tp', tp; 'fp', fp; 'fn', fn;
-    'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-display(x);
-
-
-tp = 0;
-fp = 0;
-fn = 0;
-tn = 0;
-
-
-for i = 1:length(adaboost_predict_label_2)
-    if (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 1)
-        tp = tp + 1;
-    elseif (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 0)
-        fn = fn + 1;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 1)
-        fp = fp + 1    ;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 0)
-        tn = tn + 1;
-    end 
-        
-end
-
-precision = tp / (tp + fp);
-
-recall =  tp / (tp + fn);
-
-accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-x = {'model 2', ''; 
-    'tp', tp; 'fp', fp; 'fn', fn;
-    'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-display(x);
-
-
-
-tp = 0;
-fp = 0;
-fn = 0;
-tn = 0;
-
-
-for i = 1:length(adaboost_predict_label_3)
-    if (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 1)
-        tp = tp + 1;
-    elseif (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_1(i) == 0)
-        fn = fn + 1;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 1)
-        fp = fp + 1    ;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_1(i) == 0)
-        tn = tn + 1;
-    end 
-        
-end
-
-precision = tp / (tp + fp);
-
-recall =  tp / (tp + fn);
-
-accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-x = {'model 3', ''; 
-    'tp', tp; 'fp', fp; 'fn', fn;
-    'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-display(x);
-
-
-
-tp = 0;
-fp = 0;
-fn = 0;
-tn = 0;
-
-
-for i = 1:length(adaboost_predict_label_4)
-    if (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_4(i) == 1)
-        tp = tp + 1;
-    elseif (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_4(i) == 0)
-        fn = fn + 1;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_4(i) == 1)
-        fp = fp + 1    ;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_4(i) == 0)
-        tn = tn + 1;
-    end 
-        
-end
-
-precision = tp / (tp + fp);
-
-recall =  tp / (tp + fn);
-
-accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-x = {'model 4', ''; 
-    'tp', tp; 'fp', fp; 'fn', fn;
-    'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-display(x);
-
-
-
-
-
-
-tp = 0;
-fp = 0;
-fn = 0;
-tn = 0;
-
-
-for i = 1:length(adaboost_predict_label_5)
-    if (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_5(i) == 1)
-        tp = tp + 1;
-    elseif (cfos_test_label_vector(i) == 1) && (adaboost_predict_label_5(i) == 0)
-        fn = fn + 1;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_5(i) == 1)
-        fp = fp + 1    ;
-    elseif (cfos_test_label_vector(i) == 0) && (adaboost_predict_label_5(i) == 0)
-        tn = tn + 1;
-    end 
-        
-end
-
-precision = tp / (tp + fp);
-
-recall =  tp / (tp + fn);
-
-accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-x = {'model 5', ''; 
-    'tp', tp; 'fp', fp; 'fn', fn;
-    'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-display(x);
-%% plot
-cfos_mislabel = check_mislabeled(cfos_test_image_path, cfos_test_label_vector, adaboost_predict_label_1);
-cfos_mislabel_2 = check_mislabeled(cfos_test_image_path, cfos_test_label_vector, adaboost_predict_label_2);
-cfos_mislabel_3 = check_mislabeled(cfos_test_image_path, cfos_test_label_vector, adaboost_predict_label_3);
-cfos_mislabel_4 = check_mislabeled(cfos_test_image_path, cfos_test_label_vector, adaboost_predict_label_4);
-cfos_mislabel_5 = check_mislabeled(cfos_test_image_path, cfos_test_label_vector, adaboost_predict_label_5);
-
-
-%%
+output = mean(result,2);
+disp(output);
 
 
