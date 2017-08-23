7c60085725dbcbed5736afb1405c20737cfd2200
qingdai <qingdai54@gmail.com>
2017-08-23 02:17:35 -0700

    add ROC, PR curve, test image random sampling

--- main_CNN.m
+++ main_CNN.m
@@ -1,19 +1,6 @@
-%% load images
-% digitDatasetPath = fullfile(matlabroot,'toolbox','nnet','nndemos', ...
-%         'nndatasets','DigitDataset');
-% digitData = imageDatastore(digitDatasetPath, ...
-%         'IncludeSubfolders',true,'LabelSource','foldernames');
-% CountLabel = digitData.countEachLabel;
-% 
 
 
-%% set default
 
-BW_patch_vector = [];
-Gray1_patch_vector = [];
-Gray2_patch_vector = [];
-
-label_vector = [];
 
 
 %%
@@ -25,7 +12,7 @@ label_vector = [];
 
 %% load images
 
-% select multiple image and tag files
+% select multiple image and tag files2
 % [filename,pathname] = uigetfile('../images/new/DH/data/cfos/*.tif', ...
 %     'Select image file', 'MultiSelect', 'on' );
 [filename,pathname] = uigetfile('../data/DH/cfos/*.tif', ...
@@ -37,40 +24,74 @@ cfos_image_path_vector = strcat(pathname, filename(:));
 [filename,pathname] = uigetfile('../data/DH/tag/*.xlsx', ...
     'Select image file', 'MultiSelect' , 'on');
 tag_path_vector = strcat(pathname, filename(:));
+%% 
+% randomly select 10 as test images from all images 
+m = length(cfos_image_path_vector);
 
+% track images by their indices
+test_index = randsample(1:m, 10);
+
+% store test images and test tag path in a vector
+test_image_path_vector = cfos_image_path_vector(test_index);
+test_tag_path_vector = tag_path_vector(test_index);
+
+% store train images and test tag path in a vector
+train_image_path_vector = setdiff(cfos_image_path_vector,test_image_path_vector );
+train_image_tag_vector = setdiff(tag_path_vector,test_tag_path_vector );
+
+%% 
+
+training_Label_vector = [];      
+training_BW_patch_vector = [];
+training_Gray_patch_vector = [];
 
 % extract patches from each image, extract labels
-l = length(filename);
 tic
-for i = 1: l
+for i = 1: length(train_image_path_vector)
     % track time
-    remain_time = (toc / i) * (l-i);
+    remain_time = (toc / i) * (length(train_image_path_vector)-i);
     
-    s = sprintf(' %d / %d \n time used: %.2f \n time remains %.2f \n', i,l, toc, remain_time);    fprintf(s);   
+    s = sprintf(' %d / %d \n time used: %.2f \n time remains %.2f \n', i,length(train_image_path_vector), toc, remain_time);    fprintf(s);   
     fprintf(s);  
     
     % extract patches from each image
-    [ ~, ~, ~, labels, BW_patch, ~, Gray2_patch]...                       
-        = create_pixel_features(cfos_image_path_vector{i}, tag_path_vector{i}, 'cfos');
+    [ labels, BW_patch, ~, Gray_patch]...                       
+        = create_pixel_features(train_image_path_vector{i}, train_image_tag_vector{i}, 'cfos');
 
-    BW_patch_vector = [BW_patch_vector   BW_patch];
-    Gray2_patch_vector = [Gray2_patch_vector   Gray2_patch];         
-    label_vector = [label_vector;  labels];      
-
+      
+    
+    
+    training_BW_patch_vector = [training_BW_patch_vector   BW_patch];
+    training_Gray_patch_vector = [training_Gray_patch_vector   Gray_patch];  
+    training_Label_vector = [training_Label_vector;  labels];      
+    
 end
     
  %% create data structure
 
 % store all patches and corresponding in imageData 
-imageData = struct('BW', BW_patch_vector, 'Gray', Gray2_patch_vector, ...
-    'label', label_vector);  
+imageData = struct('BW', training_BW_patch_vector, 'Gray', training_Gray_patch_vector, ...
+    'Label', training_Label_vector);  
+% save('/Users/qingdai/Desktop/fos_detection/data/imageData.mat' , ...
+%     'imageData');
+%% load imageData
 
 
+% 
+% feature_path = '/Users/qingdai/Desktop/fos_detection/data/imageData.mat';
+% a = load(feature_path);
+% imageData = a.imageData;
+% 
+training_BW_patch_vector = imageData.BW;
+training_Gray_patch_vector = imageData.Gray;
 
-m = length(BW_patch_vector); 
+training_Label_vector = imageData.Label;      
+
+%% cross validation
+m = length(training_BW_patch_vector); 
 
 % find the indices of all positive signals
-positive_index = find(label_vector);
+positive_index = find(training_Label_vector);
 
 % find the indices of all negative signals
 negative_index = setdiff(1:m, positive_index);
@@ -78,19 +99,16 @@ negative_index = setdiff(1:m, positive_index);
 
 % randomly choose subsamples to balance data
 % posInd = randsample(positive_index, 2500);
-% negInd = randsample(negative_index, 2500);
 negInd = randsample(negative_index, length(positive_index));
+% negInd = randsample(negative_index, length(positive_index));
 
 
 % combine and shuffle subsample
-% total_index = [posInd; negInd'];
-% total_index = [positive_index; negInd'];
-% total_index=total_index(randperm(length(total_index)));
-total_index = [positive_index; negative_index'];
+total_index = [positive_index; negInd'];
+% total_index = [positive_index; negative_index'];
 
 total_index = total_index(randperm(length(total_index)));
 
-subtotal_index = total_index(1:5000);
 
 
 % set k-fold validation
@@ -112,16 +130,16 @@ inputlayer = imageInputLayer([41, 41, 1] );
 
 % convolutional layer
 % filter size and Filters are random
-convlayer = convolution2dLayer([4,4],10,'Stride',1);
+convlayer = convolution2dLayer([4,4],7,'Stride',1);
 
-convlayer2 = convolution2dLayer([4,4],10,'Stride',1);
+convlayer2 = convolution2dLayer([4,4],7,'Stride',1);
 
 % RELU layer
 relulayer = reluLayer();
 
 % pooling layer
 maxpoollayer = maxPooling2dLayer([2,2], 'Stride', 1);
-% avgpoollayer = averagePooling2dLayer([2,2], 'Stride', 1);
+avgpoollayer = averagePooling2dLayer([2,2], 'Stride', 1);
 
 % dropout layer
 droplayer = dropoutLayer();
@@ -139,13 +157,14 @@ coutputlayer = classificationLayer();
 % construct layers
 layers = [inputlayer
           convlayer
-%           relulayer
-%           convlayer2
-%           relulayer
+          relulayer
+%            convlayer2
+%            relulayer
           maxpoollayer
 %           avgpoollayer
-%           droplayer
-          fullconnectlayer
+          droplayer
+%           fullconnectlayer
+           fullconnectlayer
           smlayer
           coutputlayer];
       
@@ -154,34 +173,37 @@ layers = [inputlayer
 
 
 %% learn
-result = zeros(6,5);
+result = zeros(9,k);
+
 tic
 for i = 1:k
     % divide into two subsets by indices
     trainInd = total_index(training(cv,i));
-    testInd  = total_index(test(cv,i));
+    valInd  = total_index(test(cv,i));
 
 
 %     trainInd = subtotal_index(training(cv,i));
 %     testInd  = subtotal_index(test(cv,i));
     
     % IMPROVE: get from patch_vector by indices to save stack space
-    trainData = struct('BW', imageData.BW(trainInd(1:end)), 'Gray', ...
-      imageData.Gray(trainInd(1:end)), 'label', imageData.label(trainInd(1:end)));                                   
+    
+    
+    trainData = struct('BW', training_BW_patch_vector(trainInd), 'Gray', ...
+      training_Gray_patch_vector(trainInd), 'Label', training_Label_vector(trainInd));                                   
 
-    testData = struct('BW', imageData.BW(testInd(1:end)), 'Gray', ...
-      imageData.Gray(testInd(1:end)), 'label', imageData.label(testInd(1:end)));  
+    valData = struct('BW', training_BW_patch_vector(valInd(1:end)), 'Gray', ...
+      training_Gray_patch_vector(valInd), 'Label', training_Label_vector(valInd));  
   
   
-  % create 4-D image array (height, width, channel, index)		 +  % create 4-D image array
-   % X: training data, 		
-  % Y: traning label    
-  X = zeros(41,41,1,length(trainData.BW));
+    % create 4-D image array (height, width, channel, index)		 +  % create 4-D image array
+    % X: training data, 		
+    % Y: traning label    
+    X = zeros(41,41,1,length(trainData.BW));
     for j = 1:length(trainData.BW)
         X(:,:,1, j) = trainData.BW(j).image;
     end
 
-    Y = categorical(trainData.label);
+    Y = categorical(trainData.Label);
 
     
     functions = { ...
@@ -195,16 +217,15 @@ for i = 1:k
 %          'LearnRateDropPeriod',5, 'MaxEpochs',10, 'OutputFcn',functions);
 %     
   % specify training options		
-    options = trainingOptions('sgdm', 'OutputFcn',functions);
-    
+    options = trainingOptions('sgdm', 'InitialLearnRate',0.008);
+
     % tracking
     s = sprintf('%d / %d ', i, k);
     fprintf(s); 
     
     
     % train net
-    trainedNet = trainNetwork(X, Y,layers,options);
-
+    trainedNet = trainNetwork(X, Y, layers,options);
 % time tracking
     remain_time = (toc / i) * (k-i);
     s = sprintf('time used: %.2f \n time remains: %.2f \n', toc, remain_time);
@@ -212,76 +233,149 @@ for i = 1:k
   
   
   
-  % test
+  % Validation
     
-  % XTest: testing data
-    XTest = zeros(41,41,1,length(testData.BW));
+  % XVal: testing data
+    XVal = zeros(41,41,1,length(valData.BW));
     
-    for j = 1:length(testData.BW)
-        XTest(:,:,1, j) = testData.BW(j).image;
+    for j = 1:length(valData.BW)
+        XVal(:,:,1, j) = valData.BW(j).image;
     end
 
 
 
     % predict
-    % YTest is predict label
-    % TTest is true label
-    [YTest,scores] = classify(trainedNet,XTest);
-    TTest = categorical(testData.label);
+    % YVal is predict label
+    % TVal is true label
+    [YVal,score] = classify(trainedNet,XVal);
+    TVal = categorical(valData.Label);
 
+   
+    % ROC curve
+    [X,Y,T,AUC,OPTROCPT,SUBY] = perfcurve(valData.Label,score(:,2), 1, 'XCrit','fall', 'YCrit','sens');
+    figure;plot(X,Y);
     
+     % Precision/Recall curve
+    [X2,Y2,T,AUC2,OPTROCPT,SUBY] = perfcurve(valData.Label,score(:,2), 1, 'XCrit','reca', 'YCrit','prec');
+    figure;plot(X2,Y2);
     
-    
-    
-    
+    predict_labels = double(YVal);
+    predict_labels(predict_labels == 1) = 0;
+    predict_labels(predict_labels == 2) = 1;
+
     %  peformance 
     
-    tp = 0;
-    fp = 0;
-    fn = 0;
-    tn = 0;
-    
+    [r, f1_score] = check_accuracy(valData.Label, predict_labels);
 
-    for j = 1:length(YTest)
-        if (TTest(j) == categorical(1)) && (YTest(j) == categorical(1))
-            tp = tp + 1;
-        elseif (TTest(j) == categorical(1)) && (YTest(j) == categorical(0))
-            fn = fn + 1;
-        elseif (TTest(j) == categorical(0)) && (YTest(j) == categorical(1))
-            fp = fp + 1    ;
-        elseif (TTest(j) == categorical(0)) && (YTest(j) == categorical(0))
-            tn = tn + 1;
-        end 
+    result(:,i) =  [r(1:end); AUC; AUC2; f1_score];
 
-    end
-
-    precision = tp / (tp + fp);
-
-    recall =  tp / (tp + fn);
-
-    accuracy = (tp + tn) / (tp + tn + fp + fn );
-
-
-    x = {'CNN', ''; 
-        'tp', tp; 'fp', fp; 'fn', fn;
-        'precision: ', precision; 'recall: ', recall; 'accuracy: ', accuracy};
-
-
-   r = cell2mat(x(2:7,2));
-   disp(r);
-  result(:, i) = r;		
+    	
   
   
 end
                      
-output = mean(result,2);
-disp(output);
+disp('avg');
+avg = mean(result,2);
+disp(avg);
 
     
    
- 
+ %% save model
+
+save('/Users/qingdai/Desktop/fos_detection/model/model_CNN(Gray, I_equalized).mat' , ...
+    'trainedNet');
+
+save('/Users/qingdai/Desktop/fos_detection/data/imageData(60).mat' , ...
+    'imageData');
 
 
+predict_model = trainedNet;
+
+%% %% test model
+
+% initialization
+test_Features = [];
+test_Labels = [];
+predict_Labels = [];
+score = [];
+result = [];
+
+for i = 1:length(test_image_path_vector)
+   disp(i)
+   
+   % extract features and labels from the vectors created in the second
+   % section
+    [ Labels, BW_patch, ~, Gray_patch]...                       
+        = create_pixel_features(test_image_path_vector{i}, test_tag_path_vector{i}, 'cfos');
+         
+    % concatenate true labels among iterations
+    test_Labels = [test_Labels; Labels];
+    
+    
+    XTest = zeros(41,41,1,length(BW_patch));
+    
+    for j = 1:length(BW_patch)
+        XTest(:,:,1, j) = BW_patch(j).image;
+    end
+
+
+
+    % predict
+    % YVal is predict label
+    % TVal is true label
+    [YTest,s] = classify(trainedNet,XTest);
+    YTest = double(YTest);
+    YTest(YTest == 1) = 0;
+    YTest(YTest == 2) = 1;
+
+    % concatenate predict labels among iterations
+    predict_Labels = [predict_Labels ;YTest];
+   
+    % concatenate score/probability among iterations
+    score = [score; s];
+    
+    % check test accuracy
+    % r stores tp, fp, fn, precision, recall, accuracy,f1_score and print to the
+    % console
+    [r, f1_score] = check_accuracy(Labels, YTest);
+    r = [r;f1_score];
+    result = [result r]; 
+
+    % total number of positive candidates predicted 
+    num_of_predict(i) = length(find(YTest));
+    
+    % total number of true positive candidates
+    num_of_true(i) = length(find(Labels));
+    
+
+
+end
+
+% plot true/predicted number of positive signals
+x = 1:10;
+figure;plot(x,num_of_predict, x, num_of_true );
+xlabel('image index')
+ylabel('number of positive signals')
+
+avg = mean(result, 2);
+
+
+
+% ROC Curve
+% TPR vs FPR = sensitivity vs (1-specificity) = TP/(TP+FN) vs FP/(FP+TN)
+[X,Y,T,AUC,OPTROCPT,SUBY] = perfcurve(test_Labels,score(:,2), 1,'XCrit','fall', 'YCrit','sens');
+figure;plot(X,Y);
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC Curve')
+
+% Precision/Recall curve
+% TP/(TP+FP) vs TP/(TP+FN) 
+[X2,Y2,T,AUC2,OPTROCPT,SUBY] = perfcurve(test_Labels,score(:,2), 1, 'XCrit','reca', 'YCrit','prec');
+figure;plot(X2,Y2);
+xlabel('Recall')
+ylabel('Precision')
+title('PR curve')
 
 
 
